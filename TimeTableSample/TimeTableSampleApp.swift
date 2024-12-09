20b148482405c4df127b44a301f13ff72f027787
Takuya Aso <milanista224@gmail.com>
2024-12-09 23:35:58 +0900

    Initial Commit

---
+++ TimeTableSample/TimeTableSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTableSampleApp.swift
+//  TimeTableSample
+//
+//  Created by Takuya Aso on 2024/12/09.
+//
+
+import SwiftUI
+
+@main
+struct TimeTableSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
