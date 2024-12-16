afeb6740dc764d34d76690e1c3fcd97c9c7f2f67
Takuya Aso <milanista224@gmail.com>
2024-12-16 13:54:51 +0900

    #2 時刻表を取得したい駅選択のUI実装

--- TimeTableSample/ContentView.swift
+++ TimeTableSample/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedStation: SelectableStations?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                Menu {
+                    ForEach(SelectableStations.allCases, id: \.self) { station in
+                        Button {
+                            selectedStation = station
+                        } label: {
+                            HStack {
+                                station.icon
+
+                                Text(station.name)
+                            }
+                        }
+                    }
+
+                } label: {
+                    if let selectedStation = selectedStation {
+                        HStack {
+                            selectedStation.icon
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 36.0, height: 36.0)
+
+                            Text(selectedStation.name)
+                                .font(.title2)
+                                .bold()
+                        }
+                    } else {
+                        Text("時刻表を取得したい駅を選択")
+                            .foregroundStyle(.blue)
+                    }
+                }
+                .tint(.black)
+                .padding(.vertical, 64.0)
+            }
+            .navigationTitle("時刻表取得")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
