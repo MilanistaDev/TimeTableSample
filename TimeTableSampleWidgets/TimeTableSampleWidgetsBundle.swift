e90d024462e68c7fc12cbc5e44586c6ce9c274c3
Takuya Aso <milanista224@gmail.com>
2024-12-20 15:57:57 +0900

    #9 Widget Extension ターゲット追加

---
+++ TimeTableSampleWidgets/TimeTableSampleWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimeTableSampleWidgetsBundle.swift
+//  TimeTableSampleWidgets
+//
+//  Created by Takuya Aso on 2024/12/20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimeTableSampleWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        TimeTableSampleWidgets()
+    }
+}
