ba215e18e8000bcb17eed4803e8cf723d5737da2
Takuya Aso <milanista224@gmail.com>
2024-12-21 15:48:39 +0900

    #13 WidgetsのUI実装

--- TimeTableSampleWidgets/TimeTableSampleWidgets.swift
+++ TimeTableSampleWidgets/TimeTableSampleWidgets.swift
@@ -74,9 +74,78 @@ struct TimeTableSampleWidgetsEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
+        VStack(spacing: .zero) {
+            HStack(spacing: 8.0) {
+                entry.timeTable.first?.station.icon
+                    .frame(width: 36.0, height: 36.0)
+
+                Text(entry.timeTable.first?.station.name ?? "--")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+            }
+            .padding(.bottom, 4.0)
+
+            HStack(spacing: .zero) {
+                ForEach(entry.timeTable, id: \.railDirection) { data in
+                    VStack(alignment: .leading, spacing: .zero) {
+                        Text(data.railDirection.direction)
+                            .font(.footnote)
+                            .bold()
+                            .foregroundStyle(.white)
+                            .padding(.leading, 8.0)
+                            .padding(.bottom, 4.0)
+
+                        ForEach(data.timeTable, id: \.trainNumber) { timeInfo in
+                            HStack(spacing: 4.0) {
+                                Text(timeInfo.departureTime)
+                                    .font(.caption)
+                                    .bold()
+                                    .monospacedDigit()
+                                    .foregroundStyle(.white)
+                                    .padding(.leading, 2.0)
+
+                                Text(timeInfo.trainType.name)
+                                    .frame(width: 20.0, height: 20.0)
+                                    .font(.caption2)
+                                    .lineLimit(2)
+                                    .minimumScaleFactor(0.1)
+                                    .foregroundStyle(.white)
+
+                                timeInfo.destinationStation.first?.icon
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20.0, height: 20.0)
+
+                                Text(timeInfo.destinationStation.first?.destination ?? "--")
+                                    .font(.caption)
+                                    .bold()
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .minimumScaleFactor(0.1)
+                            }
+                        }
+                        .padding(.horizontal, 4.0)
+                        .padding(.vertical, 2.0)
+                        .background(.black)
+                    }
+                }
+            }
+
+            Spacer()
+        }
+        .padding(.all, 16.0)
+        .frame(maxWidth: .infinity)
+        .background(Color("navyBlue"))
+        .overlay(alignment: .topTrailing) {
+            Text(entry.timeTable.first?.dayType.name ?? "--")
+                .font(.caption2)
+                .foregroundStyle(.white)
+                .padding(.all, 4.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4.0)
+                        .stroke(.white, lineWidth: 1.0)
+                )
+                .padding(.all, 20.0)
         }
     }
 }
